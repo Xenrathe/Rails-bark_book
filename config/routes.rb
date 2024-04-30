980cac2fed409c6c3356bf51d1fd0924d92ac75a
Xenrathe <126534032+Xenrathe@users.noreply.github.com>
2024-04-30 16:11:12 -0400

    Add confirmable to users and update styling on devise views (password, sessions, confirmation, etc)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations', omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_for :users, controllers: { confirmations: 'users/confirmations', sessions: 'users/sessions', registrations: 'users/registrations', omniauth_callbacks: 'users/omniauth_callbacks' }
 
   resources :users, except: [:index] do
     member do
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_04_16_201712) do
+ActiveRecord::Schema[7.0].define(version: 2024_04_27_174807) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -169,6 +169,10 @@ ActiveRecord::Schema[7.0].define(version: 2024_04_16_201712) do
     t.string "bark_sound", default: "mute"
     t.string "provider"
     t.string "uid"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["primary_address_id"], name: "index_users_on_primary_address_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
