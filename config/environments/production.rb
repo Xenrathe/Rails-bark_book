daf856ba2e280b589b739706d2e44005e7a4e4ed
Xenrathe <126534032+Xenrathe@users.noreply.github.com>
2024-05-07 09:33:20 -0400

    Adjust some of the automailer/sendgrid parameters to make e-mail seem less spammy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,7 +75,7 @@ Rails.application.configure do
     :authentication => :plain,
     :user_name => 'apikey', # This is the string literal 'apikey', NOT the ID of your API key
     :password => ENV['SENDGRID_API_KEY'], # This is the secret sendgrid API key which was issued during API key creation
-    :domain => 'em3236.www.barkpups.com',
+    :domain => 'barkpups.com',
     :enable_starttls_auto => true
   }
 
