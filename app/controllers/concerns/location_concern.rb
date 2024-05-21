c50775006ff3a3c91991595631c724e2ee35538e
Xenrathe <126534032+Xenrathe@users.noreply.github.com>
2024-05-20 23:00:23 -0400

    Alter how get_location finds IP to correctly (i hope) obtain it from fly.io's request headers

--- app/controllers/concerns/location_concern.rb
+++ app/controllers/concerns/location_concern.rb
@@ -5,7 +5,7 @@ module LocationConcern
   def get_location(user)
     location_cookie = cookies[:user_location]
 
-    # Priority is return browser location api
+    # Priority is to return browser location API
     # ELSE return user's primary address (which should be geocoded)
     # ELSE return IP
     if location_cookie.present?
@@ -14,19 +14,30 @@ module LocationConcern
     elsif user&.primary_address
       user.primary_address
     else
-      remote_ip = request.remote_ip
-      ip_address = IPAddr.new(remote_ip)
-      
-      # Convert IPv6 to IPv4 if necessary
-      if ip_address.ipv4?
-        proper_ip = remote_ip
-      elsif ip_address.ipv6? && ip_address.ipv4_mapped?
-        proper_ip = ip_address.ipv4_mapped.to_s
+      # Retrieve the IP address from the Fly-Client-IP header
+      fly_client_ip = request.headers['HTTP_FLY_CLIENT_IP']
+      remote_ip = fly_client_ip || request.remote_ip
+      ip_address = IPAddr.new(remote_ip) rescue nil
+
+      # Ensure we have a valid IP address
+      if ip_address
+        # Convert IPv6 to IPv4 if necessary
+        if ip_address.ipv4?
+          proper_ip = remote_ip
+        elsif ip_address.ipv6? && ip_address.ipv4_mapped?
+          proper_ip = ip_address.native.to_s
+        else
+          proper_ip = nil
+        end
       else
         proper_ip = nil
       end
-      
-      puts "PROPER IP is: #{proper_ip}"
+
+      puts "REMOTE IP: #{remote_ip}"
+      puts "FLY CLIENT IP: #{fly_client_ip}"
+      puts "IP ADDRESS: #{ip_address.inspect}"
+      puts "PROPER IP: #{proper_ip}"
+
       proper_ip
     end
   end
