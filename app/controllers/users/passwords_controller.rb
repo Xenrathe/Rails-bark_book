2986c6f4c4732e4ff07cacecce87868dd55f1ac2
Erik Rollwage <126534032+Xenrathe@users.noreply.github.com>
2025-07-02 09:19:56 -0400

    Add protections against mis-use of the password reset function

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -7,9 +7,26 @@ class Users::PasswordsController < Devise::PasswordsController
   # end
 
   # POST /resource/password
-  # def create
-  #   super
-  # end
+  def create
+    # A hidden honeypot field to catch bots
+    if params[:nickname].present?
+      Rails.logger.info "Honeypot triggered on password reset by IP #{request.remote_ip}"
+      flash[:notice] = "If this email address is linked to an account, an email with instructions will arrive in a few minutes."
+      redirect_to new_session_path(resource_name) and return
+    end
+
+    #check if user email actually exists
+    user = resource_class.find_by(email: params[resource_name][:email])
+
+    if user
+      super
+      flash[:notice] = "If this email address is linked to an account, an email with instructions will arrive in a few minutes."
+    else
+      Rails.logger.info "Non-existent email request from IP #{request.remote_ip}"
+      flash[:notice] = "If this email address is linked to an account, an email with instructions will arrive in a few minutes."
+      redirect_to new_session_path(resource_name) and return
+    end
+  end
 
   # GET /resource/password/edit?reset_password_token=abcdef
   # def edit
